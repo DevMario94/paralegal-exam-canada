5373d7aef3347f9524a4b440605d211f21adf15d
Mario Lolli <music.lpg@gmail.com>
2026-06-05 00:46:22 -0400

    Simplify: just pod install

--- ios/App/ci_scripts/ci_post_clone.sh
+++ ios/App/ci_scripts/ci_post_clone.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-export PATH="/usr/local/bin:/opt/homebrew/bin:/usr/bin:/bin:$PATH"
 set -e
-cd "$CI_PRIMARY_REPOSITORY_PATH"
-npm install
-cd ios/App
+cd "$CI_PRIMARY_REPOSITORY_PATH/ios/App"
 pod install
